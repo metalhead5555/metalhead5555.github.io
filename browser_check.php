c0e9142770a2d0167c8ec5d645fc691543fb18e4
supermegaadmin <metalhead5555@gmail.com>
2015-02-25 22:47:10 +0200

    one more commit

--- browser_check.php
+++ browser_check.php
@@ -1,5 +1,5 @@
 <?php  
 	if ( strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6.0')||strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 7.0')
 	||strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 8.0')||strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 9.0')) 
-	header("Location:badbrowser.html");
+	header("Location:index.html");
 ?>
